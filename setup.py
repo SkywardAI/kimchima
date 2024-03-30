fd2a5893f7dcd6d3d6f4671e16de55641dcf32fb
Aisuko <urakiny@gmail.com>
2024-03-30 10:18:01 +0000

    release new version
    Signed-off-by: GitHub <noreply@github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kimchima',
-    version='0.1.0',
+    version='0.2.0',
     author='Aisuko',
     author_email='urakiny@gmail.com',
     description='A collection of tools for testing and dump llama2',
