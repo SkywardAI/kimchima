9d9116441b704958a3c781d0405964ad1eee9818
Aisuko <urakiny@gmail.com>
2024-04-21 11:17:26 +0000

    add dialog and summarize pipeline
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- src/kimchima/tests/test_dialog.py
+++ src/kimchima/tests/test_dialog.py
@@ -52,3 +52,38 @@ class TestChatSummary(unittest.TestCase):
             # res is str and should not be None
             self.assertIsNotNone(res)
             self.assertIsInstance(res, str)
+
+        
+        def test_dialog_with_pipe(self):
+            """
+            Test dialog with pipe method
+            """
+            con = PipelinesFactory.init_conversation()
+            con.add_message({"role": "user", "content": "Dod you like weather of Melbourne?"})
+            con.add_message({"role": "assistant", "content": "Melbourne is also sunny which is my favourite weather"})
+            con.add_message({"role": "user", "content": "why Melbourne is a good place to travel?"})
+            pipe=Dialog.dialog_with_pipe(pipe_con=self.pipe_con, messages=con)
+
+            # pipe is list and should not be None
+            self.assertIsNotNone(pipe)
+
+
+        def test_summary_with_pipe(self):
+            """
+            Test summary with pipe method
+            """
+            paragraph="""
+                    Melbourne, the vibrant capital of Victoria,
+                    Australia, pulsates with a captivating blend of culture, art, and sport. 
+                    Its laneways are adorned with striking street art, while grand Victorian-era buildings stand as testaments to its rich history. 
+                    The city boasts world-class museums, like the Melbourne Museum and the National Gallery of Victoria,
+                      alongside bustling markets and hidden bars waiting to be discovered. 
+                      Sports enthusiasts revel in the electric atmosphere of the Melbourne Cricket Ground and Rod Laver Arena, 
+                      hosting iconic events like the Australian Open and the Formula 1 Grand Prix. With its diverse culinary scene, 
+                      renowned coffee culture, and thriving nightlife, Melbourne offers an unforgettable experience for every visitor."""
+            pipe=PipelinesFactory.customized_pipe(model=self.summarization_model, device_map='auto')
+            res=Dialog.summary_with_pipe(summary_pipe=pipe, paragraph=paragraph, max_length=10)
+
+            # res is str and should not be None
+            self.assertIsNotNone(res)
+            self.assertIsInstance(res, str)
